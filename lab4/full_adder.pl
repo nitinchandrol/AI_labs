b64989accff5abd75be1a58c71c1f66f454310c3
whichrakesh <whichrakesh@gmail.com>
2015-03-21 16:18:07 +0530

    added circuit break and short circuiting

--- lab4/full_adder.pl
+++ lab4/full_adder.pl
@@ -1,6 +1,6 @@
 full_adder(Y):- 
-	connected(in(1,Y),in(1,x1)), connected(in(2,Y),in(2,x1)), connected(in(1,Y),in(1,a1)), connected(in(2,Y),in(2,a1)), xor(x1), and(a1), connected(out(x1),in(1,x2)),connected(in(3,Y),in(2,x2)), xor(x2), connected(out(x2),out(2,Y)), connected(out(x1), in(1,a2)), connected(in(3,Y), in(2,a2)), and(a2), connected(out(a1), in(2,o1)), connected(out(a2),in(1,o1)), or(o1), connected(out(o1),out(1,Y)), my_retract(x1), my_retract(x2), my_retract(a1), my_retract(a2), my_retract(o1).
+	connected(in(1,Y),in(1,x1)), grounded(in(1,x1)), connected(in(2,Y),in(2,x1)), connected(in(1,Y),in(1,a1)), connected(in(2,Y),in(2,a1)), xor(x1), and(a1), connected(out(x1),in(1,x2)),connected(in(3,Y),in(2,x2)), xor(x2), connected(out(x2),out(2,Y)), connected(out(x1), in(1,a2)), connected(in(3,Y), in(2,a2)), and(a2), connected(out(a1), in(2,o1)), connected(out(a2),in(1,o1)), or(o1), connected(out(o1),out(1,Y)), my_retract(x1), my_retract(x2), my_retract(a1), my_retract(a2), my_retract(o1),!.
 
-add([X,Y,Z],[A,B]):- assert(signal(in(1,ad1),X)), assert(signal(in(2,ad1),Y)), assert(signal(in(3,ad1),Z)), full_adder(ad1), signal(out(1,ad1),A), signal(out(2,ad1),B), my_add_retract(ad1),!.
 
 my_add_retract(G):- retract(signal(in(1,G),_)), retract(signal(in(2,G),_)), retract(signal(in(3,G),_)), retract(signal(out(1,G),_)),  retract(signal(out(2,G),_)).
+
--- lab4/general.pl
+++ lab4/general.pl
@@ -1,5 +1,4 @@
 :- dynamic signal/2.
-
 or(X):- low(in(1,X)),low(in(2,X)),!, assert(signal(out(X),0)).
 or(X):- assert(signal(out(X),1)).
 
@@ -10,16 +9,30 @@ not(X) :- low(in(1,X)),!,assert(signal(out(X),1)).
 not(X) :- high(in(1,X)),!,assert(signal(out(X),0)).
  
 
-low(T1) :- signal(T1,0).
-high(T1) :- signal(T1,1).
+low(T1) :- notshorted(T1), signal(T1,0).
+high(T1) :- notshorted(T1), signal(T1,1).
 
+grounded(T1):- assert(signal(T1,0)),!.
 
-connected(T1,T2):- low(T1),!,assert(signal(T2,0)).
-connected(T1,T2):- high(T1),!,assert(signal(T2,1)).
+signalled(T1):- signal(T1,0),!.
+signalled(T1):- signal(T1,1),!.
+signalled(_):- write('connection missing\n'),!,fail. 
+
+notshorted(T1):- signal(T1,0), signal(T1,1),write('shorted\n'),!,fail.
+notshorted(T1):- signal(T1,1), signal(T1,0),write('shorted\n'),!,fail.
+notshorted(_).
+
+connected(T1,T2):- signalled(T1), low(T1),!,assert(signal(T2,0)).
+connected(T1,T2):- signalled(T1), high(T1),!,assert(signal(T2,1)).
 
 my_not_retract(G):- retract(signal(in(_,G),_)), retract(signal(out(G),_)).
 
 my_retract(G) :- retract(signal(in(1,G),_)), retract(signal(in(2,G),_)), retract(signal(out(G),_)).
 
+verify(and,[X1,X2],Y):- assert(signal(in(1,x),X1)), assert(signal(in(2,x),X2)), and(x), signal(out(x),Y), my_retract(x).
+verify(or,[X1,X2],Y):- assert(signal(in(1,x),X1)), assert(signal(in(2,x),X2)), or(x), signal(out(x),Y), my_retract(x).
+verify(not,X1,Y):- assert(signal(in(1,x),X1)), not(x), signal(out(x),Y), my_not_retract(x).
 
+verify(xor,[X1,X2],Y):- assert(signal(in(1,x),X1)), assert(signal(in(2,x),X2)), xor(x), signal(out(x),Y), my_retract(x).
 
+verify(full_adder, [X,Y,Z],[A,B]):- assert(signal(in(1,ad1),X)), assert(signal(in(2,ad1),Y)), assert(signal(in(3,ad1),Z)), full_adder(ad1), signal(out(1,ad1),A), signal(out(2,ad1),B), my_add_retract(ad1),!.
